ab5f5988c3d45910dbf6c8c7960f7eb80f254780
Galym <38518853+GGnet123@users.noreply.github.com>
2019-09-19 21:23:17 +0600

    Add files via upload

--- SiteController.php
+++ SiteController.php
@@ -3,18 +3,14 @@
 namespace app\controllers;
 
 use app\models\Coordinates;
-use Yii;
-use yii\filters\AccessControl;
 use yii\web\Controller;
-use yii\web\Response;
-use yii\filters\VerbFilter;
-use app\models\LoginForm;
-use app\models\ContactForm;
+
+
 
 class SiteController extends Controller
 {
     public function actionIndex(){
-        $model = Coordinates::find()->one();
+        $model = Coordinates::find()->orderBy('id DESC')->limit(1)->one();
         return $this->render('index',[
             'model' => $model
         ]);
--- index.php
+++ index.php
@@ -1,78 +1,18 @@
 <script>
-    var map, infoWindow;
-    function addMarker(location, map) {
+    function initMap() {
+        var mapOptions = {
+            zoom: 18,
+            center: new google.maps.LatLng(<?php echo $model->lat; echo ','; echo $model->lng; ?>),
+            mapTypeId: 'roadmap'
+        };
+        var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+        var position = {<?php echo 'lat:'. $model->lat; echo ','; echo 'lng:'.$model->lng; ?>};
         var marker = new google.maps.Marker({
-            position: location,
-            map: map
+            position: position,
+            map: map,
+            title: 'Your Location'
         });
-    }
 
-
-
-    var request = new XMLHttpRequest();
-
-    request.open('GET', ' http://ac3e8a0a.ngrok.io/', true);
-
-    var data = JSON.parse(this.response);
-    request.onload = function() {
-        console.log(data);
-
-    };
-
-
-    request.send();
-    //var arr = http.get('http://6f3bf001.ngrok.io');
-    function initMap(<?php echo 'latt='. $model->lat; echo ','; echo 'lngg='.$model->lng; ?>) {
-
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: latt,
-                    lng: lngg},
-            zoom: 3
-        });
-        infoWindow = new google.maps.InfoWindow;
-
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(position) {
-                var pos = {
-					lat: latt,
-                    lng: lngg
-                };
-
-                addMarker(pos,map);
-              
-                map.setCenter(pos);
-            }, function() {
-                handleLocationError(true, infoWindow, map.getCenter());
-            });
-        } else {
-
-            handleLocationError(false, infoWindow, map.getCenter());
-        }
-		$(document).ready(function(){
-		  $(latt).change(function(){
-			alert("The text has been changed.");
-			navigator.geolocation.getCurrentPosition(function(position) {
-                var pos = {
-					lat: latt,
-                    lng: lngg
-                };
-                addMarker(pos,map);
-
-                map.setCenter(pos);
-            }, function() {
-                handleLocationError(true, infoWindow, map.getCenter());
-            })
-		  });
-		});
-
-		
-	}
-
-
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-            addMarker(pos,map) :
-            'Error: Your browser doesn\'t support geolocation.');
     }
 </script>
